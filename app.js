3d8ea8b0a7d6b7f223bcfde8df4f8d4c26414d56
Azat Abrarov <ufasat@yandex.ru>
2021-07-07 00:57:33 +0500

    update eslint config

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+const helmet = require('helmet');
 
 const { PORT = 3000 } = process.env;
 
@@ -13,7 +15,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useCreateIndex: true,
   useFindAndModify: false,
 });
-
+app.use(helmet());
 app.use((req, res, next) => {
   req.user = {
     _id: '60e3443ec4111347c4ca13d2',
