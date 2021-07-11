af2969a42af823954dc9d2e5898a3f1fc6d7cb93
Azat Abrarov <ufasat@yandex.ru>
2021-07-11 17:38:25 +0500

    hotfix/catch cards errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,10 +2,14 @@ const Card = require('../models/card');
 
 const addCard = (req, res) => {
   const data = { ...req.body, owner: req.user._id };
-
   return Card.create(data)
     .then((card) => res.status(200).send(card))
-    .catch(() => res.status(400).send({ message: 'Переданы некорректные данные' }));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      }
+      res.status(500).send({ message: err.message });
+    });
 };
 
 const getCards = (req, res) => {
@@ -17,40 +21,53 @@ const getCards = (req, res) => {
 const deleteCard = (req, res) => {
   const id = req.params.cardId;
   return Card.findByIdAndRemove(id)
-    .then((card) => {
-      if (!card) {
-        return res.status(404).send({ message: 'Нет карточки с таким id' });
+    .orFail(new Error('NotValidId'))
+    .then((card) => res.status(200).send(card))
+    .catch((err) => {
+      if (err.name === 'NotValidId') {
+        return res.status(404).send({ message: 'Карточка с указанным _id не найдена' });
+      } if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные' });
       }
-      return res.status(200).send(card);
-    })
-    .catch((err) => res.status(500).send({ message: err.message }));
+      return res.status(500).send({ message: err.message });
+    });
 };
 
-const putLike = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $addToSet: { likes: req.user._id } },
-  { new: true },
-)
-  .then((card) => {
-    if (!card) {
-      return res.status(404).send({ message: 'Нет пользователя с таким id' });
-    }
-    return res.status(200).send(card);
-  })
-  .catch((err) => res.status(500).send({ message: err.message }));
+const putLike = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail(new Error('NotValidId'))
+    .then((card) => res.status(200).send(card))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные' });
+      } if (err.name === 'NotValidId') {
+        return res.status(404).send({ message: 'Нет пользователя с таким id' });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
 
-const removeLike = (req, res) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } },
-  { new: true },
-)
-  .then((card) => {
-    if (!card) {
-      return res.status(404).send({ message: 'Нет пользователя с таким id' });
-    }
-    return res.status(200).send(card);
-  })
-  .catch((err) => res.status(500).send({ message: err.message }));
+const removeLike = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .orFail(new Error('NotValidId'))
+    .then((card) => res.status(200).send(card))
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Переданы некорректные данные' });
+      } if (err.name === 'NotValidId') {
+        return res.status(404).send({ message: 'Нет пользователя с таким id' });
+      }
+      return res.status(500).send({ message: err.message });
+    });
+};
 
 module.exports = {
   addCard, getCards, deleteCard, putLike, removeLike,
